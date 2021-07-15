433e853ee1da26f6af6980396971b03c7060a6a3
michalarent <arent.michal@gmail.com>
2021-07-15 23:41:06 +0200

    1st

---
+++ components/Overlay.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { a } from "@react-spring/web";
+import useWindowSize from "./useWindowSize.js";
+
+export default function Overlay({ fill }) {
+  // Just a Figma export, the fill is animated
+  const size = useWindowSize();
+  return (
+    <div className="overlay">
+      <a.svg
+        style={{ width: "100vw", height: "100vh" }}
+        fill={fill}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <text
+          style={{ whiteSpace: "pre" }}
+          fontFamily="Inter"
+          fontSize={size.width*0.03}
+          fontWeight="normal"
+          letterSpacing="-.02em"
+        >
+          <tspan
+            x={size.width / 3 + size.width / 4}
+            y={size.height / 4 - size.height/24}
+            children="ART ENGINEERING"
+          />
+        </text>
+        <text
+          style={{ whiteSpace: "pre" }}
+          fontFamily="Inter"
+          fontSize={size.width*0.02}
+          fontWeight="bold"
+          letterSpacing="-.02em"
+        >
+          <tspan
+            x={size.width / 3 + size.width / 4}
+            y={size.height / 4}
+            children="ARENT.MICHAL<@>GMAIL.COM "
+          />
+        </text>
+      </a.svg>
+    </div>
+  );
+}
